e320ee8601f2eb66f70a85afd502c7fb45bc144b
josephna76 <59426134+josephna76@users.noreply.github.com>
2024-02-17 18:11:11 -0800

    Fix attempt update ui

--- js/uiUpdater.js
+++ js/uiUpdater.js
@@ -11,39 +11,39 @@ const uiUpdater = (() => {
         task.notes.length > 0 ? `Notes: ${task.notes.join(", ")}` : "No notes";
 
       taskItem.innerHTML = `
-            <div>
-              <input type="checkbox" id="task-${task.id}" ${
+              <div>
+                <input type="checkbox" id="task-${task.id}" ${
         task.completed ? "checked" : ""
       } onchange="toggleTaskCompleted(${task.id})"/>
-              <label for="task-${task.id}">${task.name} - Deadline: ${
+                <label for="task-${task.id}">${task.name} - Deadline: ${
         task.deadline
       }
-              </label>
-              <div>${notes}</div>
-              <select id="priority-${task.id}" ${
+                </label>
+                <div>${notes}</div>
+                <select id="priority-${task.id}" ${
         task.completed ? "disabled" : ""
       }>
-                <option value="High" ${
-                  task.priority === "High" ? "selected" : ""
-                }>High</option>
-                <option value="Medium" ${
-                  task.priority === "Medium" ? "selected" : ""
-                }>Medium</option>
-                <option value="Low" ${
-                  task.priority === "Low" ? "selected" : ""
-                }>Low</option>
-              </select>
-              <button onclick="updateTaskPriority(${task.id})" ${
+                  <option value="High" ${
+                    task.priority === "High" ? "selected" : ""
+                  }>High</option>
+                  <option value="Medium" ${
+                    task.priority === "Medium" ? "selected" : ""
+                  }>Medium</option>
+                  <option value="Low" ${
+                    task.priority === "Low" ? "selected" : ""
+                  }>Low</option>
+                </select>
+                <button onclick="updateTaskPriority(${task.id})" ${
         task.completed ? "disabled" : ""
       }>Update Priority</button>
-              <button onclick="deleteTask(${task.id})">Delete</button>
-              ${
-                !task.completed
-                  ? `<button onclick="editTask(${task.id})">Edit</button>`
-                  : ""
-              }
-            </div>
-          `;
+                <button onclick="deleteTask(${task.id})">Delete</button>
+                ${
+                  !task.completed
+                    ? `<button onclick="editTask(${task.id})">Edit</button>`
+                    : ""
+                }
+              </div>
+            `;
 
       taskList.appendChild(taskItem);
     });
